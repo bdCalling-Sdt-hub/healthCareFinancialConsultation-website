aab520594405225c4431e4e0725cd0da437a1b59
Mahmud Hasan Sabbir <mahmoodsabbir3087@gmail.com>
2025-05-16 16:10:52 +0600

    refactor: update API and image URLs to use HTTPS
    Updated the base API URL and image URL to use HTTPS for improved security and reliability. The previous HTTP URLs have been commented out for reference.

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -4,7 +4,8 @@ const api = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
     // baseUrl: "http://10.0.80.49:5020/api/v1",
-    baseUrl: "http://52.52.50.140:5000/api/v1",
+    // baseUrl: "http://52.52.50.140:5000/api/v1",
+    baseUrl: "https://api.hcfinconsults.com/api/v1",
     prepareHeaders: (headers) => {
       const token =
         localStorage.getItem("authenticationToken") ||
@@ -23,4 +24,5 @@ const api = createApi({
 export const { reducer } = api;
 export default api;
 // export const imageUrl = "http://10.0.80.49:5020";
-export const imageUrl = "http://52.52.50.140:5000";
+// export const imageUrl = "http://52.52.50.140:5000";
+export const imageUrl = "https://api.hcfinconsults.com";
